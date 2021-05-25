f7c343d70f91b93c66c9fd8733613c3ef8336f4e
Andre Menezes <djdedemenezes@gmail.com>
2021-05-25 20:18:25 -0300

    CRUD Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/articles", to: "articles#index", as: :articles
+  root to: "articles#index"
 
+  get "/articles", to: "articles#index", as: :articles
+  
   get "/articles/new", to: "articles#new", as: :new_article
+    
   post "/articles", to: "articles#create"
+
+  get "/article/:id/edit", to: "articles#edit", as: :edit_article
+
+  patch "/article/:id", to: "articles#update"
+
+  get "/article/:id", to: "articles#show", as: :article
+
+  delete "/article/:id", to: "articles#destroy"
+
+
 
 end
